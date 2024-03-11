bf66f683b7ebbe282ceeac6bc7ede592687e7436
Sajjad <sajad.soojoodi@gmail.com>
2024-03-11 14:12:25 +0330

    add to git

--- app/src/main/java/ir/sajjad/iranitour/AboutUsFragment.kt
+++ app/src/main/java/ir/sajjad/iranitour/AboutUsFragment.kt
@@ -5,16 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ir.sajjad.iranitour.databinding.FragmentSavedLocationBinding
+import ir.sajjad.iranitour.databinding.FragmentAboutUsBinding
 
-
-class SavedLocationFragment : Fragment() {
-    lateinit var binding: FragmentSavedLocationBinding
-
+class AboutUsFragment : Fragment() {
+    lateinit var binding: FragmentAboutUsBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentSavedLocationBinding.inflate(layoutInflater)
+        binding  = FragmentAboutUsBinding.inflate(layoutInflater)
         return binding.root
     }
-
-
 }
--- app/src/main/java/ir/sajjad/iranitour/activity/MainActivity.kt
+++ app/src/main/java/ir/sajjad/iranitour/activity/MainActivity.kt
@@ -1,8 +1,13 @@
 package ir.sajjad.iranitour.activity
 
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import ir.sajjad.iranitour.R
 import ir.sajjad.iranitour.databinding.ActivityMainBinding
@@ -29,9 +34,17 @@ class MainActivity : AppCompatActivity() {
         actionBarDrawerToggle.syncState()
         binding.navigationViewMain.setNavigationItemSelectedListener {
             when(it.itemId){
-                R.id.menu_save_locations ->{}
-                R.id.menu_setting ->{}
+                R.id.menu_save_locations ->{
+                    findNavController(R.id.fragmentContainerView).navigate(R.id.savedLocationFragment)
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_setting ->{
+                }
                 R.id.menu_about_us ->{}
+
+                R.id.menu_exit ->{
+                    finish()
+                }
             }
             true
         }
